a5f447ba85552f1eec5d518c572bc26d2f91f68f
Kurakin Alexander <kuraga333@mail.ru>
2013-01-31 15:10:34 +0400

    `TwitterWidget`: refactor

--- app/widgets/twitter_widget.rb
+++ app/widgets/twitter_widget.rb
@@ -2,18 +2,18 @@ class TwitterWidget < Apotomo::Widget
   responds_to_event :submit, :with => :process_tweet
 
   def display
-    @tweets = Tweet.all
+    @tweets = Tweet.find(:all)
     render
   end
   
   def process_tweet(evt)
     Tweet.new(:text => evt[:text]).save
 
-    @tweets = Tweet.all
-    replace :view => :display
+    replace :state => :display
   end
-
+  
   def list(tweets)
     render :locals => {:tweets => tweets}
   end
+  
 end

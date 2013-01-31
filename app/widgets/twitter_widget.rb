69e035e0d4ebffd95d3fdc41a027c504d338950f
Kurakin Alexander <kuraga333@mail.ru>
2013-01-31 12:55:17 +0400

    `TwitterWidget`: add `display` and `process_tweet` states

--- app/widgets/twitter_widget.rb
+++ app/widgets/twitter_widget.rb
@@ -1,7 +1,15 @@
 class TwitterWidget < Apotomo::Widget
+  responds_to_event :submit, :with => :process_tweet
 
   def display
+    @tweets = Tweet.all
     render
   end
+  
+  def process_tweet(evt)
+    Tweet.new(:text => evt[:text]).save
 
+    @tweets = Tweet.all
+    replace :view => :display
+  end
 end
